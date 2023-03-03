4b9233b0db6fd2a2c56c826bfdb6022165082112
rover0811 <rover0811@hotmail.com>
2023-03-04 01:49:11 +0900

    [docs] 실행방법 docs 추가

--- GDSC_server.py
+++ GDSC_server.py
@@ -4,7 +4,9 @@ from GDSC import predict_image_classification_sample as predict
 from PIL import Image
 import io
 app = FastAPI()
-
+# 꼭 GDSC_Solution_Challenge 폴더로 이동하고, 
+# uvicorn GDSC_server:app --reload 로 실행
+# 127.0.0.1/docs 로 들어가서 스웨거 테스트
 
 
 @app.post("/uploadfile/")
